3ad1bc9365c82249860967a7cbe0a6b25f76245c
Sebastian Lehrack <sebastian.lehrack@cognostics.de>
2020-12-25 19:53:52 +0100

    day 24 part one done, preparations for part two

--- twentyfour/flipping_tiles.py
+++ twentyfour/flipping_tiles.py
@@ -8,9 +8,11 @@ Created on 25/12/2020 14:10
 flipping_tiles.py is a script for advent of code day 24
 """
 
-from math import sqrt
 import os
+from math import sqrt, acos, pi, degrees, cos, sin, radians
+
 from common import load_input_file
+
 
 class Vector:
     """a 2dimensional vektor class"""
@@ -28,8 +30,11 @@ class Vector:
     def cross_product(self, other):
         pass
 
+    def dot_product(self, other):
+        return self.a * other.a + self.b * other.b
+
     def __abs__(self):
-        return round(sqrt(self.a * self.a + self.b * self.b))
+        return sqrt(self.a * self.a + self.b * self.b)
 
     def __add__(self, other):
         return Vector(self.a + other.a, self.b + other.b)
@@ -37,21 +42,85 @@ class Vector:
     def __str__(self):
         return f'({self.a}, {self.b})'
 
+    def angle(self, other):
+        if abs(other) == 0 or abs(self) == 0:
+            return 0
+        if self.b < other.b:
+            sign = -1
+        else:
+            sign = 1
+        return round(acos((self.dot_product(other)) / round((abs(self) * abs(other)), 5)), 5)
 
-wurzel_drei_halbe = sqrt(3)*0.5
-NORTH_EAST = Vector(wurzel_drei_halbe, wurzel_drei_halbe)
-NORT_WEST = Vector(-1*wurzel_drei_halbe, wurzel_drei_halbe)
-SOUTH_EAST = Vector(wurzel_drei_halbe, -1*wurzel_drei_halbe)
-SOUTH_WEST = Vector(-1*wurzel_drei_halbe, -1*wurzel_drei_halbe)
+    def direction(self):
+        if self.b == 0:
+            if self.a >= 0:
+                return 0
+            if self.a < 0:
+                return pi
+        if self.b < 0:
+            return 2 * pi - round((acos(self.a / abs(self))), 5)
+        return round((acos(self.a / abs(self))), 5)
+
+
+wurzel_drei_halbe = sqrt(3) * 0.5
+NORTH_EAST = Vector(0.5, wurzel_drei_halbe)
+NORTH_WEST = Vector(-0.5, wurzel_drei_halbe)
+SOUTH_EAST = Vector(0.5, -1 * wurzel_drei_halbe)
+SOUTH_WEST = Vector(-0.5, -1 * wurzel_drei_halbe)
 WEST = Vector(-1, 0)
 EAST = Vector(1, 0)
 
+
+def get_tile_pos(line):
+    line_iter = iter(line)
+    value = next(line_iter, 0)
+    tile_position = Vector(0, 0)
+    while value != 0:
+        if value == 'e':
+            tile_position += EAST
+        elif value == 'w':
+            tile_position += WEST
+        elif value == 'n':
+            value = next(line_iter)
+            if value == 'e':
+                tile_position += NORTH_EAST
+            elif value == 'w':
+                tile_position += NORTH_WEST
+        elif value == 's':
+            value = next(line_iter)
+            if value == 'e':
+                tile_position += SOUTH_EAST
+            elif value == 'w':
+                tile_position += SOUTH_WEST
+        value = next(line_iter, 0)
+    return round(degrees(tile_position.direction()), 3), round(abs(tile_position), 5)
+
+
 def flipping_tiles_from_line(input_lines):
-    pass
+    tile_dict = {}
+    for line in input_lines:
+        # the tile position is a tuple of angle and length
+        tile_direction, tile_magnitude = get_tile_pos(line)
+        # prepare directions for 360deg symmetry
+        # direction is mod pi and negative for values larger 180
+
+        tile_position = (tile_direction, tile_magnitude)
+        tile_dict[tile_position] = (tile_dict.get(tile_position, 0) + 1) % 2
+    return sum(tile_dict.values())
+
+
+def get_neightbors(tile, tile_pos):
+    # from given tuple of direction and magnitude, reconstruct tile vector
+    tile_vector = Vector(tile[1] * cos(radians(tile[0])), tile[1] * sin(radians(tile[0])))
+    return_tile_dict = {}
+    for adjecent in [EAST, WEST, NORTH_EAST, NORTH_WEST, SOUTH_EAST, SOUTH_WEST]:
+        new_tile = tile_vector + adjecent
+        new_tile_pos = (new_tile.direction(), abs(new_tile))
+        return_tile_dict[new_tile_pos] = tile_pos.get(new_tile_pos, 0)
+    return return_tile_dict
 
 
 if __name__ == "__main__":
     print("Welcome to flipping_tiles.py")
     input_lines = load_input_file(os.getcwd())
     print(flipping_tiles_from_line(input_lines))
-
--- twentyfour/tests/test_flipping_tiles.py
+++ twentyfour/tests/test_flipping_tiles.py
@@ -8,19 +8,26 @@ Created on 25/12/2020 14:09
 test_flipping_tiles.py is a unittest for advent of code day 24/
 """
 
-import unittest
 import os
-from common import load_input_file
-from twentyfour import flipping_tiles
+import unittest
 
+from common import load_input_file
+from twentyfour.flipping_tiles import flipping_tiles_from_line, get_tile_pos, SOUTH_EAST
+from math import radians
 
 class TestFlippingTiles(unittest.TestCase):
 
     def setUp(self) -> None:
         self.input_lines = load_input_file(os.getcwd(), 'test_input.txt')
 
+    def test_directions(self):
+        self.assertEqual((300, 1.0), get_tile_pos('esew'))
+        self.assertEqual((0, 0), get_tile_pos('nwwswee'))
+
     def test_part_one(self):
-        self.assertEqual(10, flipping_tiles(self.input_lines))
+        self.assertEqual(10, flipping_tiles_from_line(self.input_lines))
+        real_input = load_input_file(os.path.join(os.getcwd(), '..'))
+        self.assertEqual(485, flipping_tiles_from_line(real_input))
 
 
 if __name__ == "__main__":
